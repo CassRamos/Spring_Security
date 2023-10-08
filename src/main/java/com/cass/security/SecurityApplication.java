dfbbe09734d2007a24649b1089a5ed7d3bb82147
Cass <cassianoleaoramos@gmail.com>
2023-10-08 18:32:27 -0300

    initial commit

---
+++ src/main/java/com/cass/security/SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.cass.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityApplication.class, args);
+    }
+
+}
