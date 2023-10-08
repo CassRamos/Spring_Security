f7be24dbdf970aa786dfe858e2ba0d1d6043a404
Cass <cassianoleaoramos@gmail.com>
2023-10-08 19:47:18 -0300

    finishing logout service

---
+++ src/main/java/com/cass/security/config/LogoutService.java
@@ -0,0 +1,36 @@
+package com.cass.security.config;
+
+import com.cass.security.token.TokenRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogoutService implements LogoutHandler {
+
+    private final TokenRepository tokenRepository;
+
+    //this method is responsible for invalidating the token, so when the user logs out for the application they won't have access with the same token
+    @Override
+    public void logout(HttpServletRequest request,
+                       HttpServletResponse response,
+                       Authentication authentication) {
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        if (authHeader == null || !authHeader.startsWith("Bearer")) {
+            return;
+        }
+        jwt = authHeader.substring(7);
+        var storedToken = tokenRepository.findByToken(jwt)
+                .orElse(null);
+        if (storedToken != null) {
+            storedToken.setRevoked(true);
+            storedToken.setExpired(true);
+            tokenRepository.save(storedToken);
+        }
+    }
+}
